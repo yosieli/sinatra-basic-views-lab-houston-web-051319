49a6c5adf13080d92b77370820b2c66cca35221f
feven tsegay <feventsegay6@gmail.com>
2019-06-03 16:31:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+    configure do
+        set :views, "views"
+        set :public_dir, "public"
+    end
+  
+    get "/" do
+        erb :index
+    end
 
 end
